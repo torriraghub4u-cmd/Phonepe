766fdff8fc8932dc4a641dd570c38579f5ae9499
RaghavendraT <torriraghub4u@gmail.com>
2026-07-22 12:17:42 +0530

    up

---
+++ story_content/5l2UQw26mpp_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5l2UQw26mpp_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.116.36838.0%0D%0A%0D%0A00%3A00%3A00.075%20--%3E%2000%3A00%3A01.525%0D%0ALaunch%20the%20PhonePe%20app%0D%0A%0D%0A"}]})
---
+++ story_content/6KJB2A5lvCb_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6KJB2A5lvCb_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.116.36838.0%0D%0A%0D%0A00%3A00%3A00.075%20--%3E%2000%3A00%3A01.765%0D%0ASelect%20the%20linked%20bank%20account%0D%0A%0D%0A"}]})
